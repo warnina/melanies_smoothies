5b1d763e18891eccadcac9f6eeff443eb46a2b3b
warnina <159792386+warnina@users.noreply.github.com>
2025-03-21 15:03:48 -0400

    Update streamlit_app.py
    removed watermelon section at bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,3 @@ if ingreditents_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
