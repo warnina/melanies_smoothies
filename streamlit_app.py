d9a9808428cea2c8794f18bda2a4c5712041acf0
warnina <159792386+warnina@users.noreply.github.com>
2025-03-21 15:55:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 pd_df=my_dataframe.to_pandas()
-# st.dataframe(pd_df)
+st.dataframe(pd_df)
 # st.stop()
 
 ingreditents_list = st.multiselect(
