7ffe0394374da5047f9c6a7a40f2fab67c0e3c3c
warnina <159792386+warnina@users.noreply.github.com>
2025-03-21 16:04:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,12 @@ st.write("The name on your Smoothie will be", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
+# st.dataframe(pd_df)
 # st.stop()
 
 ingreditents_list = st.multiselect(
@@ -34,7 +34,6 @@ if ingreditents_list:
 
     for fruit_chosen in ingreditents_list:
         ingredients_string += fruit_chosen + ' '
-
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
