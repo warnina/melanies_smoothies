3be01f03c8b59ac859c09e8f6b2dad474220d663
warnina <159792386+warnina@users.noreply.github.com>
2025-03-21 14:55:26 -0400

    Update streamlit_app.py
    changed wider to width

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if ingreditents_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_wider=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
